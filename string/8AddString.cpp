cd3fda8fcf4f3b97ddf58ff348dd12d46d543704
Manoj Kumar <manoj.kumar@rain.us>
2022-06-09 15:11:40 +0530

    Add Binary string

---
+++ string/8AddString.cpp
@@ -0,0 +1,34 @@
+//
+// Created by Manoj Kumar on 09/06/22.
+//
+#include <iostream>
+#include <vector>
+#include <string>
+
+
+using namespace std;
+
+
+string solve(string a, string b){
+    int m=a.length()-1;
+    int n=b.length()-1;
+    int carry=0;
+    string res;
+
+    while (m >=0 || n >=0 ){
+        int p=(m>=0)?a[m--]-'0':0;
+        int q=(n>=0)?b[n--]-'0':0;
+        int sum=p+q+carry;
+        res= to_string(sum%2)+res;
+        carry=sum/2;
+    }
+    return carry==1?("1"+res):res;
+
+}
+
+int main(){
+    string a="100";
+    string b="11";
+    cout << solve(a,b)<< endl;
+
+}
