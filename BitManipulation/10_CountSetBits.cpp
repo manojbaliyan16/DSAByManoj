33b854b9ad7d3c625188af0df0a45c8e193b73f6
Manoj Kumar <manoj.kumar@rain.us>
2022-06-22 11:55:57 +0530

    fix the hard case Fail case

--- BitManipulation/10_CountSetBits.cpp
+++ BitManipulation/10_CountSetBits.cpp
@@ -4,64 +4,31 @@
 //
 
 #include <iostream>
-#include <vector>
-#include <unistd.h>
-
 using namespace std;
 
-
-// THIS IS THE NAVIE SOLUTION WILL GIVE TIME LIMIT EXCEED
-/*
-int Solve(uint32_t A){
-    int count=0;
-    for(int i=1;i<=A;i++){
-        int num=i;
-        while(num!=0)
-        {
-            if((num&1)==1){
-                count++;
-            }
-             num=num>>1;
-        }
-    }
-
-    return count;
-}
- */
-/*
- * 1. Calculate the LargestPowerOf2intragne (1<<(x-1))*x
- * 2. bitTill2x = n-(1<<x)+1
- * 3.find the rest number of bits and solve for the rest bits
- *
- *
- * */
-
-
-int largestPowerof2inRange(int n){
+long int largestPowerof2inRange(int n){
     int x=0;
     while((1<<x)<=n){
         x++;
     }
-    cout << "Value of x is " << x-1 << " "<< __LINE__ << endl;
     return x-1;
 }
-int countSetBits(int n){
-    const unsigned int M = 1000000007;
-    // first find the largest power of 2 whose value is less than or equal to n
+
+long int countSetBits(int n){
+    const unsigned int M = 1000000007;// first find the largest power of 2 whose value is less than or equal to n
     if(n==0){
         return 0;
     }
-    int x= largestPowerof2inRange(n); //x=powerof2
-    int numberofbitsTillmaxPowerof2=x*(1<<(x-1))%M;
-    int msb2xton= n-(1<<x)+1;
+    long int x= largestPowerof2inRange(n); //x=powerof2
+    long int numberofbitsTillmaxPowerof2=x*(1<<(x-1));
+    long int msb2xton= (n-(1<<x)+1);
 
-    int restInteger = n-(1<<x);
-    int ans=numberofbitsTillmaxPowerof2 +msb2xton + countSetBits(restInteger);
-    ans%=M;
-    return ans;
+    long int restInteger = n-(1<<x);
+    long int ans=numberofbitsTillmaxPowerof2 %M +msb2xton + countSetBits(restInteger) ;
 
-
+    return ans%M;
 }
+
 int main(){
     int A=10000;
     cout << "Value of A is " << A << " "<< __LINE__ << endl;
