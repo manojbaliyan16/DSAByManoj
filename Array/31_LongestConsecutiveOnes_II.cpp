9fe56e388cd1f9e6b73585bd50a7c2028edc69a3
Manoj Kumar <manoj.kumar@rain.us>
2022-07-03 20:00:43 +0530

    Update the solution approach

--- Array/31_LongestConsecutiveOnes_II.cpp
+++ Array/31_LongestConsecutiveOnes_II.cpp
@@ -3,7 +3,10 @@
 
 //
 /*Given a binary array, find the maximum number of consecutive 1s in this array if you can flip at most one 0. */
-
+/* WhenEver we encounter 0, we can flip to 1 and this can participate to increase the count for 1's, In this case when we encounter 0 still we have to accumulate the
+ * 1's count and then we need one more variable current to save the current value of count, after accumulating its value reset cnt to 0
+ * In this case cnt can always be used to count the pure consecutive 1s, then we can use it to update the final result  */
+//TC = O(n)
 
 #include <iostream>
 #include <vector>
