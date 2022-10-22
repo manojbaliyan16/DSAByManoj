e018f1ee944cedc9e4f28ba2ed5f7e4f7c3c1bf5
Manoj Kumar <manoj.kumar@rain.us>
2022-10-22 17:12:13 +0530

    Correction

--- BitManipulation/5-reverseTheInteger.cpp
+++ BitManipulation/5-reverseTheInteger.cpp
@@ -8,7 +8,7 @@ using namespace std;
 
 uint32_t solve(uint32_t A){
     uint32_t ret=0;
-    for(int i=0;i<4;++i){
+    for(int i=0;i<32;++i){
         if(A && 1==1){
             ret = (ret << 1)+1;
         }else{
