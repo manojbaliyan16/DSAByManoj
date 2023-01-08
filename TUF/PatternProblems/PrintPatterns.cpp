eeacc1cd2a4853b9fc1d1841350039cbd273a83e
Manoj <manojbaliyan@gmail.com>
2023-01-09 02:38:02 +0530

    Add more Patterns

--- TUF/PatternProblems/PrintPatterns.cpp
+++ TUF/PatternProblems/PrintPatterns.cpp
@@ -107,53 +107,193 @@ void Print10(int n){
     Print5(n);
 }
 
+void Print11(int n){
+    // Observations: 
+    // 1. Number of rows equal to number of input
+    // 2. Even number rows start with 1
+    int start=0; 
+    for (size_t i = 0; i < n; i++)
+    {
+        if(i%2==0){
+            start=1;
+        }else{
+            start=0;
+        }
+
+        for (size_t j = 0; j <= i; j++) // right angle triagle print
+        {
+            cout << start << " ";
+            //flipt the start value as start is either 1 0r 0
+             start =1-start;
+        }
+        cout << "\n";
+        
+        
+    }
+    
+}
+
+void Print12(int n){
+    // Observation 
+    // number space number
+    // number + space + number =2*rows
+    // space always reduce by 2 
+    int space = 2*(n-1); // because when n=1 space 
+
+    for (size_t i = 1; i <= n; i++){
+        // number
+        for(int j=1;j<=i;++j){
+            cout <<j;
+        }
+        //space
+        for(int j=1;j<=space;j++){
+            cout << " ";
+        }
+        // number but in reverse order 
+        for(int j=i;j>=1;--j){
+            cout <<j;
+        }
+        cout<< "\n";
+        space -=2;
+    }
+    
+}
+
+void Print13(int n){
+     int num=1;
+    for (size_t i = 1; i <= n; i++)
+    {
+        for(int j=1;j<=i;++j){
+            cout << num << " ";
+            num++;
+        }
+        cout << "\n";
+    }
+    
+}
+void Print14(int n){
+    
+    for (size_t i = 0; i < n; i++)
+    {    char ch='A';
+        for (size_t j = 0; j <= i; j++)
+        {
+            cout << ch<< " ";
+            ch+=1;
+        }
+        cout << "\n";
+        
+    }
+    
+
+}
+
+void Print15(int n){
+    
+    for (size_t i = 0; i < n; i++)
+    {    char ch='A';
+        for (size_t j = n; j > i; j--)
+        {
+            cout << ch<< " ";
+            ch+=1;
+        }
+        cout << "\n";
+        
+    }
+    
+
+}
+void Print16(int n){
+    char ch='A'-1;
+    for (size_t i = 0; i < n; i++)
+    {
+        char ch='A'+i;
+       for(int j=0;j<=i;j++){
+        cout << ch << " ";
+       }
+       cout << "\n";
+    }
+    
+}
+void Print17(int n){
+    for (size_t i = 0; i < n; i++)
+    {   
+        //space
+        for (size_t j =0; j<n-i-1; j++)
+        {
+            cout<<" ";
+        }
+        //char 
+        char ch='A';
+        int breakPoint=(2*i+1)/2; //ABCBA
+        for(int k=1;k<=(2*i+1);k++){
+            cout << ch;
+            if(k<=breakPoint){
+                ch++;
+            }else{
+                ch--;
+            }
+            
+        }
+        //space 
+        for (size_t j =0; j<n-i-1; j++)
+        {
+            cout<<" ";
+        }
+        cout<<"\n";
+        
+    }
+    
+}
 int main(int argc, char const *argv[])
 {
     int n=5;
+    // cout << "\n";
+    // cout << "*********************1************************* ";
+    // cout << "\n";
+    // Print1(n);
+    // cout << "\n";
+    // cout << "*********************2************************* ";
+    // cout << "\n";
+     Print2(n);
+    // cout << "\n";
+    // cout << "**********************3************************ ";
+     cout << "\n";
+    // Print3(n);
+    // cout << "\n";
+    // cout << "**********************4************************ ";
+    // cout << "\n";
+    // Print4(n);
+    // cout << "\n";
+    // cout << "**********************5*********************** ";
+    // cout << "\n";
+     Print5(n);
     cout << "\n";
-    cout << "*********************1************************* ";
-    cout << "\n";
-    Print1(n);
-    cout << "\n";
-    cout << "*********************2************************* ";
-    cout << "\n";
-    Print2(n);
-    cout << "\n";
-    cout << "**********************3************************ ";
-    cout << "\n";
-    Print3(n);
-    cout << "\n";
-    cout << "**********************4************************ ";
-    cout << "\n";
-    Print4(n);
-    cout << "\n";
-    cout << "**********************5*********************** ";
-    cout << "\n";
-    Print5(n);
-    cout << "\n";
-    cout << "**********************6*********************** ";
-    cout << "\n";
-    Print6(n);
-    cout << "\n";
-    cout << "***********************7********************* ";
-    cout << "\n";
+    // cout << "**********************6*********************** ";
+    // cout << "\n";
+    // Print6(n);
+    // cout << "\n";
+    // cout << "***********************7********************* ";
+    // cout << "\n";
     Print7(n);
-    cout << "\n";
-    cout << "**********************8********************** ";
-    cout << "\n";
-    Print8(n);
-    cout << "\n";
-    cout << "*********************9*********************** ";
-    cout << "\n";
-    Print9(n);
-    cout << "\n";
-    cout << "*********************10*********************** ";
-    cout << "\n";
-    Print10(n);
-    
-   // Print1(n);
-    //Print1(n);
-
+     cout << "\n";
+    // cout << "**********************8********************** ";
+    // cout << "\n";
+    // Print8(n);
+    // cout << "\n";
+    // cout << "*********************9*********************** ";
+    // cout << "\n";
+    // Print9(n);
+    // cout << "\n";
+    // cout << "*********************10*********************** ";
+    // cout << "\n";
+    // Print10(n); 
+    // Print11(n);
+    //Print12(n);
+    //Print13(n);
+    //Print14(n);
+    //Print15(n);
+    //Print16(n);
+    Print17(n);
     
 
 
