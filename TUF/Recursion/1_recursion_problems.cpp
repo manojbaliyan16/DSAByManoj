7239ccbc897b11182fd14185e2d59f137272a0e2
Manoj <manojbaliyan@gmail.com>
2023-01-23 11:39:47 +0530

    Recursion problems added

--- TUF/Recursion/1_recursion_problems.cpp
+++ TUF/Recursion/1_recursion_problems.cpp
@@ -137,13 +137,105 @@ void reverseArray_3(vector<int> &vec){
     reverseArray_3(vec);
    
 }
+// Check If String Palindrom  // Iteratrive Optimal 
+bool IsStringPalindrom(string str){
+    int len=str.length();
+    int start=0;
+    int end=len-1;
+
+    // base condition
+ /*    if(start>=end){
+        start=0;
+        end=len-1;
+        return;
+    } */
+    while(start<end){
+        if(str[start]==str[end]){
+            start++;
+            end--;
+        }
+        else{
+            return false;
+        }
+        
+    }
+    return true;
+}
+// Recursive : IsPalindrom 
+// Time complexity depends upon the total number of recursive call and in this case total number of recursive call  will be n/2 
+// because every once start will reach to end pointer that time i function will return 
+// So total time complexity will be O(n/2) or O(n)
+// while with this recursion there will O(n) space complexity also which is due to stack call
+
+bool IsStringPalindrom_Recursive(string str){
+    int len=str.length();
+    static int start=0;
+    static int end=len-1;
+
+    // base condition
+    if(start>=end){
+
+        return true;
+    } 
+    if(str[start]!=str[end]){
+        return false;
+    }
+    start++;
+    end--;
+   return IsStringPalindrom(str);
+   
+}
+/***********************************Fibnaaci Number **************************************************/
+// Time complexity will be O(2^n)
+// because it is coming from 1. PrintFib(n-1) and 2. PrintFib(n-2)
+// So there is 2 recurive call for each call and it will increase exponentially with the number of inputs
+// for example for n=4 function makes 3 calls while N=5 function makes 5 call 
+// and obvious due to stack call there will be O(n) space complexity as well 
+// There will be more efficient ways 1. iteratrive method and 2. memorizatiom or DP 
+// here we will see Iterative mehod
+
+// Recursive 
+int PrintFib(int n){
+    if(n<=0) return 0;
+    if(n==1) return 1;
+    return PrintFib(n-1) + PrintFib(n-2);
+}
+// Iterative Method to Print the Fibnaaci Series 
+
+void PrintFib_Itertive(int n){
+    int first=0;
+    int second=1;
+    int next=0;// initialization
+    for (size_t i = 0; i < n; i++)
+    {
+        if(i<=1){
+            next=i;
+        }
+        else{
+            next=first+second;
+            first=second;
+            second=next;
+        }
+        cout << next << " ";
+    }
+    cout << "\n";
+    
+}
+
 int main(int argc, char const *argv[])
 {
-    vector<int> vec{1,2,3,4,5};
+    PrintFib_Itertive(5);
+    
+    /* for(int i=0;i<5;i++)
+    cout << PrintFib(i) << " ";
+    cout << endl; */
+    /* string str{"qmoeeomq"};
+    cout << "String str " << str << "is Palindrom " << std::boolalpha  << IsStringPalindrom(str) << endl; */
+   /*  vector<int> vec{1,2,3,4,5};
     vector<int> vec_3{2,4,6,8,10};
     reverseArray_1(vec);
     reverseArray_2(vec);
-    reverseArray_3(vec_3);
+    reverseArray_3(vec_3); */
    // cout << Factorial(7) << endl;
     //SumOfNumbers_1(5);
     //cout << endl;
